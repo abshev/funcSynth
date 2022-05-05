211ddbddc23878d573783f24e6c20b3b7662e34b
Aaron B Shev <abshev@ucdavis.edu>
2022-05-05 13:28:51 -0700

    * Added functionality to optimize V matrix over mean square error loss.
    * Added cigarette data
    
    * Fixed bug causing crashes when covariates were not used.

---
+++ R/optimize_weights.R
@@ -0,0 +1,20 @@
+
+
+optimizeWeights = function(X0, X1, V, margin.ipop = 5e-04, sigf.ipop = 5,
+                           bound.ipop = 10){
+  X0.scaled <- scale(X0)
+  X1.scaled <- scale(X1)
+  V <- diag(V)
+  H <- t(X0.scaled) %*% V %*% (X0.scaled)
+  cee <- -1 * c(t(X1.scaled) %*% V %*% (X0.scaled))
+  A <- t(rep(1, length(cee)))
+  bee <- 1
+  l <- rep(0, length(cee))
+  u <- rep(1, length(cee))
+  r <- 0
+  
+  res <- ipop(c = cee, H = H, A = A, b = bee, l = l, u = u, 
+              r = r, margin = margin.ipop, maxiter = 1000, sigf = sigf.ipop, 
+              bound = bound.ipop)
+  return(as.matrix(primal(res)))
+}
--- R/solve_for_weights.R
+++ R/solve_for_weights.R
@@ -1,24 +1,74 @@
 #' Find SCM weights using quadratic optimization
 #'
 #' @importFrom kernlab ipop
-#' 
-#'  
 
-solveForWeights = function(X0, X1, V, Margin.ipop = 5e-04, Sigf.ipop = 5,
-                           Bound.ipop = 10){
-  X0.scaled = scale(X0)
-  X1.scaled = scale(X1)
-  V <- diag(V)
-  H <- t(X0.scaled) %*% V %*% (X0.scaled)
-  c <- -1 * c(t(X1.scaled) %*% V %*% (X0.scaled))
-  A <- t(rep(1, length(c)))
-  b <- 1
-  l <- rep(0, length(c))
-  u <- rep(1, length(c))
-  r <- 0
-  
-  res <- ipop(c = c, H = H, A = A, b = b, l = l, u = u, 
-              r = r, margin = Margin.ipop, maxiter = 1000, sigf = Sigf.ipop, 
-              bound = Bound.ipop)
-  solution.w <- as.matrix(primal(res))
+solveForWeights = function(X0, X1, Z0 = NULL, Z1 = NULL, V, margin.ipop = 5e-04, 
+                           sigf.ipop = 5, bound.ipop = 10, 
+                           optimMethod = c("Nelder-Mead", "BFGS", "CG", "L-BFGS-B", "SANN",
+                                           "Brent")){
+  #Check if V is supplied
+  if(!is.null(V)){
+    w <- optimizeWeights(X0, X1, V, margin.ipop, sigf.ipop, bound.ipop)
+    return(list(V = V, w = w))
+  }
+  #If V is not supplied, determine V by optimizing over MSPE
+  else{
+    nvars = nrow(X0)
+    #Set initial value for V with equal weight
+    initialV = rep(1/nvars, times = nvars)
+    #Optimx to find V
+    rgV.optim <- optim(par = initialV, fn = lossV, gr = NULL,
+                       method = optimMethod, hessian = FALSE, 
+                          control = list(), 
+                          X0 = X0, X1 = X1, Z0 = Z0, Z1 = Z1,
+                          margin.ipop = margin.ipop, sigf.ipop = sigf.ipop, 
+                          bound.ipop = bound.ipop)
+    # if (verbose == TRUE) {
+    #   print(rgV.optim.1)
+    # }
+
+    # Xall <- cbind(X1.scaled, X0.scaled)
+    # Xall <- cbind(rep(1, ncol(Xall)), t(Xall))
+    # Zall <- cbind(Z1, Z0)
+    # Beta <- try(solve(t(Xall) %*% Xall) %*% t(Xall) %*% t(Zall), 
+    #             silent = TRUE)
+    # if (inherits(Beta, "try-error")) {
+    #   rgV.optim <- rgV.optim.1
+    # }
+    # else {
+    #   Beta <- Beta[-1, ]
+    #   V <- Beta %*% t(Beta)
+    #   SV2 <- diag(V)
+    #   SV2 <- SV2/sum(SV2)
+    #   rgV.optim.2 <- optimx(par = SV2, fn = fn.V, gr = NULL, 
+    #                         hess = NULL, method = optimxmethod, itnmax = NULL, 
+    #                         hessian = FALSE, control = list(kkt = FALSE, 
+    #                                                         starttests = FALSE, dowarn = FALSE, all.methods = all.methods), 
+    #                         X0.scaled = X0.scaled, X1.scaled = X1.scaled, 
+    #                         Z0 = Z0, Z1 = Z1, quadopt = quadopt, margin.ipop = Margin.ipop, 
+    #                         sigf.ipop = Sigf.ipop, bound.ipop = Bound.ipop)
+    #   if (verbose == TRUE) {
+    #     print(rgV.optim.2)
+    #   }
+    #   rgV.optim.2 <- collect.optimx(rgV.optim.2, "min")
+    #   if (verbose == TRUE) {
+    #     cat("\n Equal weight loss is:", rgV.optim.1$value, 
+    #         "\n")
+    #     cat("\n Regression Loss is:", rgV.optim.2$value, 
+    #         "\n")
+    #   }
+    #   if (rgV.optim.1$value < rgV.optim.2$value) {
+    #     rgV.optim <- rgV.optim.1
+    #   }
+    #   else {
+    #     rgV.optim <- rgV.optim.2
+    #   }
+    # }
+    V <- abs(rgV.optim$par)/sum(abs(rgV.optim$par))
+    w <- optimizeWeights(X0, X1, V, margin.ipop, sigf.ipop, bound.ipop)
+    return(list(V = V, w = w))
+  }
 }
+
+
+
